3d0c24d3240ab64ace9f4f3f05481d9d3b5eacbe
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-09-25 15:18:54 +0530

    feat: add product model with schema definition for title, price, and image

---
+++ src/models/product.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const productModel = mongoose.model("Products", productSchema);
+
+export default productModel;
